583cd190801438e5da4691c20655635522c0ffe6
thippo <thippo@163.com>
2015-08-12 10:19:46 +0800

    LSD one way

--- statspylib/VarianceAnalysis/ANOVA.py
+++ statspylib/VarianceAnalysis/ANOVA.py
@@ -3,7 +3,7 @@ class One_Way_ANOVA():
     import numpy as np
     import scipy.stats
     import pandas as pd
-    import matplotlib.pyplot as plt
+    import itertools
 
     def __init__(self,dataframe):
         self.data=dataframe
@@ -17,15 +17,41 @@ class One_Way_ANOVA():
         self.MSA=self.SSA/self.df_SSA
         self.MSE=self.SSE/self.df_SSE
     
-    def ANOVA(self,significance_level=0.05):
+    def ANOVA(self,significance_level=0.05,LSD=True):
         return_DataFrame=self.pd.DataFrame({'SS':[self.SSA,self.SSE,self.SST],'df':[self.df_SSA,self.df_SSE,self.df_SST],
                                            'MS':[self.MSA,self.MSE,np.NaN],'F-value':[self.MSA/self.MSE,np.NaN,np.NaN],
-                                           'F-crit':[self.scipy.stats.f.isf(significance_level,self.df_SSA,self.df_SSE),np.NaN,np.NaN]},
-                                           index=['A','E','T'],columns=['SS','df','MS','F-value','F-crit'])
+                                           'F-crit':[self.scipy.stats.f.isf(significance_level,self.df_SSA,self.df_SSE),np.NaN,np.NaN],
+                                           'result':['[accept]' if self.MSA/self.MSE<=self.scipy.stats.f.isf(significance_level,self.df_SSA,self.df_SSE) else '[refuse]',np.NaN,np.NaN],
+                                           'R2':[self.SSA/self.SST,np.NaN,np.NaN]},
+                                           index=['A','E','T'],columns=['SS','df','MS','F-value','F-crit','result','R2'])
+        print('ANOVA result')
+        print('----------------------------------------------------------------------')
         print(return_DataFrame)
-        print('R2:',self.SSA/self.SST)
-        return 'accept' if self.MSA/self.MSE<self.scipy.stats.f.isf(significance_level,self.df_SSA,self.df_SSE) else 'refuse'
-        
+        print('----------------------------------------------------------------------')
+        if LSD:
+            print()
+            print('multiple_comparison_procedures result')
+            print('----------------------------------------------------------------------')
+            self.__multiple_comparison_procedures(significance_level)
+            print('----------------------------------------------------------------------')
+        return 'accept' if return_DataFrame['F-value'][0]<return_DataFrame['F-crit'][0] else 'refuse'
+
+    def __multiple_comparison_procedures(self,significance_level):
+        return_dict={'columns1':[],'columns2':[],'statistic':[],'LSD':[],'result':[]}
+        t=self.scipy.stats.t.isf(significance_level/2,self.df_SSE)
+        combinations_list=list(self.itertools.combinations(self.data.columns,2))
+        for i,j in combinations_list:
+            return_dict['columns1'].append(i)
+            return_dict['columns2'].append(j)
+            statistic=abs(self.data[i].mean()-self.data[j].mean());return_dict['statistic'].append(statistic)
+            LSD=t*np.sqrt(self.MSE*(1/self.data[i].count()+1/self.data[j].count()));return_dict['LSD'].append(LSD)
+            return_dict['result'].append('accept' if statistic<=LSD else '*refuse')
+        return_DataFrame=self.pd.DataFrame(return_dict,columns=['columns1','columns2','statistic','LSD','result'])
+        print(return_DataFrame)
+    
+    
+    
+    
 if __name__=='__main__':
     import numpy as np
     import pandas as pd
@@ -36,5 +62,4 @@ if __name__=='__main__':
     manufacturing=[44,51,65,77,58]+[np.NaN]*2
     data=pd.DataFrame({'retail':retail,'tourism':tourism,'aircraft':aircraft,'manufacturing':manufacturing},columns=['retail','tourism','aircraft','manufacturing'])
     a=One_Way_ANOVA(data)
-    print(a.ANOVA())
-    input()
+    a.ANOVA()
